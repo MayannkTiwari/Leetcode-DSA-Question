698d251649164bf038692104c85e9dbd8b6f33f5
Mayank Tiwari <official.mayank2005t@gmail.com>
2026-09-04 19:12:47 +0530

    Time: 0 ms (100%), Space: 42 MB (70.37%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    
+        HashMap<Integer,Integer> memo = new HashMap<>();
+            public int fib(int n) {
+                if(n==0){
+                    return 0;
+                }
+                if(n==1){
+                    return 1;
+                }
+                if(memo.containsKey(n)){
+                    return memo.get(n);
+                }
+                int ans=fib(n-1)+fib(n-2);
+                memo.put(n,ans);
+                return ans;
+
+    
+        
+    }
+}
