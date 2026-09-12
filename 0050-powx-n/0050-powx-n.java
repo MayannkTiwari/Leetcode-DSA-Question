5d7551a47d8b4fbac993555f3672fe7f21c56098
Mayank Tiwari <official.mayank2005t@gmail.com>
2026-09-13 01:57:37 +0530

    Time: 0 ms (100%), Space: 48.4 MB (10.71%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,25 +1,21 @@
-class Solution {
-    public double myPow(double x, int n) {
-        long N = n;
-
-        if (N < 0) {
-            return 1 / power(x, -N);
+class Solution{
+    public double myPow(double x, int n){
+        long N =n;
+        if(N<0){
+            return 1/ power(x,-N);
         }
-
-        return power(x, N);
+        return power(x,N);
     }
-
-    private double power(double x, long N) {
-        if (N == 0) {
-            return 1;
+        private double power(double x, long N){
+            if(N==0){
+                return 1;
+            }
+            double half = power(x,N/2);
+            if(N%2==0){
+                return half*half;
+            }else{
+                return half*half*x;
+            }
         }
 
-        double half = power(x, N / 2);
-
-        if (N % 2 == 0) {
-            return half * half;
-        } else {
-            return half * half * x;
-        }
     }
-}
