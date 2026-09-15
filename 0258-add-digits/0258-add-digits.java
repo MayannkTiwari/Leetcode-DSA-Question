e0a1d7776252057719a7e614e11594b250d2015b
Mayank Tiwari <official.mayank2005t@gmail.com>
2026-09-16 00:52:29 +0530

    Time: 1 ms (98.1%), Space: 42.8 MB (25.49%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,16 +1,21 @@
+// class Solution {
+//     public int addDigits(int num) {
+//     // int sum =0;
+//     while(num>9){
+//         int sum=0;
+    
+//     while(num>0){
+//         int digit=num%10;
+//         sum=digit+sum;
+//         num=num/10;
+//     }
+//     num=sum;
+//     }
+//       return num;
+//     }
+// }
 class Solution {
     public int addDigits(int num) {
-    // int sum =0;
-    while(num>9){
-        int sum=0;
-    
-    while(num>0){
-        int digit=num%10;
-        sum=digit+sum;
-        num=num/10;
-    }
-    num=sum;
-    }
-      return num;
+        return (num - 1) % 9 + 1;
     }
 }
